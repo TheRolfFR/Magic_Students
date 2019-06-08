b69a5caaa6e0647c670bef4e593e8cad3957a330
TheRolf <yann.lv@live.fr>
2019-06-08 12:25:05 +0200

    added arrows

--- src/Entities/Projectiles/Arrow.java
+++ src/Entities/Projectiles/Arrow.java
@@ -1,13 +1,25 @@
 package Entities.Projectiles;
 
+import Renderer.ProjectileRenderer;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
 import org.newdawn.slick.geom.Vector2f;
 
 public class Arrow extends Projectile {
 
-    public Arrow(float x, float y, float maxSpeed, float accelerationRate, String imagePath, Vector2f direction, int radius) {
-        super(x, y, maxSpeed, accelerationRate, direction, imagePath, radius);
+    private static final int SIZE = 1;
+
+    public Arrow(Vector2f position, Vector2f direction) {
+        super(position.getX(), position.getY(), Snowball.MAX_SPEED*4, Snowball.ACCELERATION_RATE*4, SIZE, direction);
 
         this.updateSpeed(direction.normalise().scale(this.getAccelerationRate()));
+
+        try {
+            this.renderer = new ProjectileRenderer(this, new Image("img/arrow_32x32.png", false, Image.FILTER_NEAREST).getScaledCopy(2), new Vector2f(64, 64), 1000);
+        } catch (SlickException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
     }
 
     @Override
