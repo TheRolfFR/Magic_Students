0ef8ef4d98b68a57a17a3d9b2fb1255a93356362
TheRolf <yann.lv@live.fr>
2019-06-09 11:38:24 +0200

    corrected portal duration animation

--- src/Renderer/PortalRenderer.java
+++ src/Renderer/PortalRenderer.java
@@ -15,28 +15,30 @@ public class PortalRenderer extends SpriteRenderer {
 
     private int timeCounter;
 
-    private static final Vector2f TILESIZE = new Vector2f(32, 32);
-    private static final Vector2f TILESIZE_CENTER = TILESIZE.copy().scale(0.5f);
+    private static final float PORTAL_SCALE = 2f;
+
+    private static final Vector2f TILESIZE = new Vector2f(32, 32).scale(PORTAL_SCALE);
+    private static final Vector2f TILESIZE_OFFSET = TILESIZE.copy().scale(0.5f);
 
     private static final String PREPATH = "img/portal/";
 
-    private static final int OPENING_DURATION = 1200;
+    private static final int OPENING_DURATION = 250;
     private static final String PORTAL_OPENING_PATH = PREPATH + "portalOpening_32x32.png";
 
-    private static final int OPENED_DURATION = 100;
+    private static final int OPENED_DURATION = 750;
     private static final String PORTAL_OPENED_PATH = PREPATH + "portalOpened_32x32.png";
 
     public PortalRenderer(Entity entity, float x, float y, Color portalColor) {
         super(entity, TILESIZE);
 
         try {
-            SpriteSheet openingSpriteSheet = new SpriteSheet(new Image(PORTAL_OPENING_PATH, false, Image.FILTER_NEAREST), (int) TILESIZE.getX(), (int)TILESIZE.getY());
-            this.openingAnimation = new Animation(openingSpriteSheet, OPENING_DURATION);
+            SpriteSheet openingSpriteSheet = new SpriteSheet(new Image(PORTAL_OPENING_PATH, false, Image.FILTER_NEAREST).getScaledCopy(PORTAL_SCALE), (int) TILESIZE.getX(), (int)TILESIZE.getY());
+            this.openingAnimation = new Animation(openingSpriteSheet, OPENING_DURATION/openingSpriteSheet.getVerticalCount()/openingSpriteSheet.getHorizontalCount());
 
-            SpriteSheet openedSpriteSheet = new SpriteSheet(new Image(PORTAL_OPENED_PATH, false, Image.FILTER_NEAREST), (int) TILESIZE.getX(), (int)TILESIZE.getY());
-            this.openedAnimation = new Animation(openedSpriteSheet, OPENED_DURATION);
+            SpriteSheet openedSpriteSheet = new SpriteSheet(new Image(PORTAL_OPENED_PATH, false, Image.FILTER_NEAREST).getScaledCopy(PORTAL_SCALE), (int) TILESIZE.getX(), (int)TILESIZE.getY());
+            this.openedAnimation = new Animation(openedSpriteSheet, OPENED_DURATION/openedSpriteSheet.getVerticalCount()/openedSpriteSheet.getHorizontalCount());
 
-            this.topLeftAngle = new Vector2f(x, y).sub(TILESIZE_CENTER);
+            this.topLeftAngle = new Vector2f(x, y).sub(TILESIZE_OFFSET);
 
             this.portalColor = portalColor;
 
@@ -70,6 +72,6 @@ public class PortalRenderer extends SpriteRenderer {
             animationToShow.stop();
         }
 
-        animationToShow.draw((int) topLeftAngle.getX(), topLeftAngle.getY(), portalColor);
+        animationToShow.draw(topLeftAngle.getX(), topLeftAngle.getY(), portalColor);
     }
 }
