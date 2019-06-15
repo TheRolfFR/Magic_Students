5027b1c3a1bfa8ca692a747c47748e813ae95661
TheRolf <yann.lv@live.fr>
2019-06-15 11:44:35 +0200

    New HUD element Attack cooldown visual

--- src/Listeners/AttackCooldownListener.java
+++ src/Listeners/AttackCooldownListener.java
@@ -3,7 +3,7 @@ package Listeners;
 public interface AttackCooldownListener {
     void onCooldownStart();
 
-    void onCooldownUpdate(int currentValue, int maxValue);
+    void onCooldownUpdate(int currentValue, int maValue);
 
     void onCooldownEnd();
 }
