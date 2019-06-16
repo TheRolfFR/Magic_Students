f6725c3219ceed98f50f672e52d4b87638007737
TheRolf <yann.lv@live.fr>
2019-06-16 13:23:12 +0200

    Scaled knight renderer

--- src/Entities/LivingBeings/Monsters/Melee/KnightConstants.java
+++ src/Entities/LivingBeings/Monsters/Melee/KnightConstants.java
@@ -5,5 +5,6 @@ import org.newdawn.slick.geom.Vector2f;
 public interface KnightConstants {
     float ATTACK_LOADING_DURATION = 0.5f;
     float STUN_AFTER_ATTACK_DURATION = 0.2f;
-    Vector2f KNIGHT_TILESIZE = new Vector2f(48, 48);
+    float KNIGHT_TILEZSIZE_SCALE = 2f;
+    Vector2f KNIGHT_TILESIZE = new Vector2f(48, 48).scale(KNIGHT_TILEZSIZE_SCALE);
 }
