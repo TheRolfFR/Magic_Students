4f3d67bc391085c3fd66a7dc38810987abcf2cee
Alombaros <Alombaros@gmail.com>
2019-06-16 12:44:36 +0200

    Merge branch 'master' of https://github.com/TheRolfFR/Magic_Students
    # Conflicts:
    #	src/Entities/LivingBeings/Player.java
    
    preApocalypse

--- src/Entities/LivingBeings/Monsters/Melee/KnightConstants.java
+++ src/Entities/LivingBeings/Monsters/Melee/KnightConstants.java
@@ -2,7 +2,7 @@ package Entities.LivingBeings.Monsters.Melee;
 
 import org.newdawn.slick.geom.Vector2f;
 
-public interface KnightConstant {
+public interface KnightConstants {
     float ATTACK_LOADING_DURATION = 0.5f;
     float STUN_AFTER_ATTACK_DURATION = 0.2f;
     Vector2f KNIGHT_TILESIZE = new Vector2f(48, 48);
