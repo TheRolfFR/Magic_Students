31ae29b1fa9252cb1f462aa16b9ed1d20351ebf7
Alombaros <Alombaros@gmail.com>
2019-06-17 10:34:01 +0200

    switch place constructor / getter in entity

--- src/Entities/Entity.java
+++ src/Entities/Entity.java
@@ -14,7 +14,32 @@ public abstract class Entity {
 
     protected boolean showDebugRect;
 
+    /**
+     * Default constructor
+     */
+    public Entity() {
+        this.position = new Vector2f(0, 0);
+        this.speed = new Vector2f(0, 0);
+        this.radius = 0;
+        this.showDebugRect = false;
+        this.tileSize = new Vector2f(0, 0);
+    }
 
+    public Entity(float x, float y, int width, int height, int radius) {
+        this.position = new Vector2f(x, y);
+        this.speed = new Vector2f(0, 0);
+        this.radius = radius;
+        this.showDebugRect = false;
+        this.tileSize = new Vector2f(width, height);
+    }
+
+    public Entity(float x, float y, int radius) {
+        this.position = new Vector2f(x, y);
+        this.speed = new Vector2f(0, 0);
+        this.radius = radius;
+        this.showDebugRect = false;
+        this.tileSize = new Vector2f(radius*2, radius*2);
+    }
 
     public Vector2f getTileSize() {
         return tileSize.copy();
@@ -58,33 +83,6 @@ public abstract class Entity {
      */
     public void setShowDebugRect(boolean showDebugRect) {
         this.showDebugRect = showDebugRect;
-    }
-
-    /**
-     * Default constructor
-     */
-    public Entity() {
-        this.position = new Vector2f(0, 0);
-        this.speed = new Vector2f(0, 0);
-        this.radius = 0;
-        this.showDebugRect = false;
-        this.tileSize = new Vector2f(0, 0);
-    }
-
-    public Entity(float x, float y, int width, int height, int radius) {
-        this.position = new Vector2f(x, y);
-        this.speed = new Vector2f(0, 0);
-        this.radius = radius;
-        this.showDebugRect = false;
-        this.tileSize = new Vector2f(width, height);
-    }
-
-    public Entity(float x, float y, int radius) {
-        this.position = new Vector2f(x, y);
-        this.speed = new Vector2f(0, 0);
-        this.radius = radius;
-        this.showDebugRect = false;
-        this.tileSize = new Vector2f(radius*2, radius*2);
     }
 
     /**
